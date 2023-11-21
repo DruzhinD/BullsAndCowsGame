0120090473d2330f311de47860b8f26b14a3c233
HomePC <dimon12.dry@gmail.com>
2023-11-21 21:20:34 +1000

    Добавлен Renderer, пользовательский toolstripmenuitem

---
+++ BullsAndCows/CustomToolStripMenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace BullsAndCows
+{
+    /// <summary>
+    /// пользовательский элемент, в котором изменен его цвет при раскрытии списка его элементов,
+    /// т.е. обработано событие DropDown
+    /// </summary>
+    public class CustomToolStripMenuItem : ToolStripMenuItem
+    {
+        public CustomToolStripMenuItem()
+        {
+            this.DropDownOpening += CustomToolStripMenuItem_DropDownOpening;
+        }
+
+        private void CustomToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (this.DropDown.Visible)
+            {
+                Brush brushBack = new SolidBrush(Color.FromArgb(120, BackColor));
+                e.Graphics.FillRectangle(brushBack, 0, 0, Size.Width, Size.Height);
+
+                Brush brushFore = new SolidBrush(Color.FromArgb(255, ForeColor));
+                e.Graphics.DrawString(Text, this.Font, brushFore, new PointF(7, 1.5f));
+            }
+        }
+
+    }
+}
---
+++ BullsAndCows/MyRenderer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BullsAndCows
+{
+    /// <summary>
+    /// собственный рендер, необходимый для изменения цвета выделения
+    /// при наведении курсора мышки на элемент ToolStrip
+    /// </summary>
+    internal class MyRenderer : ToolStripProfessionalRenderer
+    {
+        private Color _backColor;
+
+        public MyRenderer(Color backColor)
+            : base()
+        {
+            this._backColor = backColor;
+        }
+
+        protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs e)
+        {
+            if (!e.Item.Selected) base.OnRenderMenuItemBackground(e);
+            else
+            {
+                Rectangle rc = new Rectangle(Point.Empty, e.Item.Size);
+
+                //сохраняем цвета в виде списка чисел
+                List<int> colorValues = new List<int> { _backColor.R, _backColor.G, _backColor.B };
+
+                if (colorValues[0] <= 255 - 50)
+                    colorValues[0] += 50;
+                else
+                    colorValues[0] = 255;
+
+                if (colorValues[1] <= 255 - 30)
+                    colorValues[1] += 30;
+                else
+                    colorValues[1] = 255;
+
+                //создаем цвета на основании имеющихся чисел
+                Color fillingColor = Color.FromArgb(colorValues[0], colorValues[1], colorValues[2]);
+                Color borderColor = Color.FromArgb(colorValues[0], colorValues[1], 255);
+
+                //создаем кисть и перо
+                Brush brushBackColor = new SolidBrush(fillingColor);
+                Pen penBorderColor = new Pen(borderColor);
+
+                //рисуем
+                e.Graphics.FillRectangle(brushBackColor, rc);
+                e.Graphics.DrawRectangle(penBorderColor, 1, 0, rc.Width - 2, rc.Height - 1);
+            }
+        }
+    }
+}
